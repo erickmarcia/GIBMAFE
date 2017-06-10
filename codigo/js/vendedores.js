3d9433931669101ebfffabbb39936bea89320182
stmendozza <stmendozza@gmail.com>
2017-06-10 13:52:40 -0500

    Add files via upload

---
+++ codigo/js/vendedores.js
@@ -0,0 +1,99 @@
+		$(document).ready(function(){
+			load(1);
+		});
+
+		function load(page){
+			var q= $("#q").val();
+			$("#loader").fadeIn('slow');
+			$.ajax({
+				url:'./ajax/buscar_vendedores.php?action=ajax&page='+page+'&q='+q,
+				 beforeSend: function(objeto){
+				 $('#loader').html('<img src="./img/ajax-loader.gif"> Cargando...');
+			  },
+				success:function(data){
+					$(".outer_div").html(data).fadeIn('slow');
+					$('#loader').html('');
+					
+				}
+			})
+		}
+
+	
+		
+			function eliminar (id)
+		{
+			var q= $("#q").val();
+		if (confirm("Realmente deseas eliminar el vendedor")){	
+		$.ajax({
+        type: "GET",
+        url: "./ajax/buscar_vendedores.php",
+        data: "id="+id,"q":q,
+		 beforeSend: function(objeto){
+			$("#resultados").html("Mensaje: Cargando...");
+		  },
+        success: function(datos){
+		$("#resultados").html(datos);
+		load(1);
+		}
+			});
+		}
+		}
+		
+		
+	
+$( "#guardar_vendedor" ).submit(function( event ) {
+  $('#guardar_datos').attr("disabled", true);
+  
+ var parametros = $(this).serialize();
+	 $.ajax({
+			type: "POST",
+			url: "ajax/nuevo_vendedor.php",
+			data: parametros,
+			 beforeSend: function(objeto){
+				$("#resultados_ajax").html("Mensaje: Cargando...");
+			  },
+			success: function(datos){
+			$("#resultados_ajax").html(datos);
+			$('#guardar_datos').attr("disabled", false);
+			load(1);
+		  }
+	});
+  event.preventDefault();
+})
+
+$( "#editar_vendedor" ).submit(function( event ) {
+  $('#actualizar_datos').attr("disabled", true);
+  
+ var parametros = $(this).serialize();
+	 $.ajax({
+			type: "POST",
+			url: "ajax/editar_vendedor.php",
+			data: parametros,
+			 beforeSend: function(objeto){
+				$("#resultados_ajax2").html("Mensaje: Cargando...");
+			  },
+			success: function(datos){
+			$("#resultados_ajax2").html(datos);
+			$('#actualizar_datos').attr("disabled", false);
+			load(1);
+		  }
+	});
+  event.preventDefault();
+})
+
+	function obtener_datos(id){
+			var documento = $("#documento"+id).val();
+			var nombre = $("#nombre"+id).val();
+			var celular = $("#celular"+id).val();
+			
+	
+			$("#mod_documento").val(documento);
+			$("#mod_nombre").val(nombre);
+			$("#mod_celular").val(celular);
+			
+		
+		}
+	
+		
+		
+
