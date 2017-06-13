c6b419f7f6b7b7c93ee1ad2c2d117dabb432973a
Cristhian Mendoza <stmendozza@gmail.com>
2017-06-13 10:56:17 -0500

    Create footer.php

--- codigo/inc/footer.php
+++ codigo/inc/footer.php
@@ -1,14 +1,13 @@
 <footer id="footer1">
 	<div class="container">
 		<div class="row">
-				<div class="col-xs-12" >
-				<center>
-				<a href="http://regionalguaviare.blogspot.com.co" target="_blank" style="color: #ecf0f1">SENA</a>	
-				<p style="color: #ecf0f1">Centro de Desarrollo Agroindustrial Turístico y Tecnológico Regional Guaviare</p>	
-				<p >Stmendozza &copy; <br>© 2017 - Boutique Maria Fernanda.</p>
-				
-				</center>
-				</div>
+			<div class="col-xs-12" >
+			<center>
+			<a href="http://regionalguaviare.blogspot.com.co" target="_blank" style="color: #ecf0f1">SENA</a>	
+			<p style="color: #ecf0f1">Centro de Desarrollo Agroindustrial Turístico y Tecnológico Regional Guaviare</p>	
+			<p style="color: yellow">Stmendozza &copy; <br>© 2017 - Boutique Maria Fernanda.</p>
+			</center>
+			</div>
 		</div>
 	</div>	
 </footer>
