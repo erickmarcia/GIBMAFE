60ce1a0871afc5b48dfa06a2bf2e1c975476a782
Cristhian Mendoza <stmendozza@gmail.com>
2017-06-19 12:54:28 -0500

    Update usuario.php

--- codigo/usuario.php
+++ codigo/usuario.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE html> 
 <html lang="es">
 <head>
 	<?php include ("inc/headcommon.php");?>
@@ -24,17 +24,76 @@
 			</div>
 			<div class="contenedor-section0	 col-xs-12 col-sm-10 col-sd-10 ">
 				    
-				    <h4 id="">Administrador</h4>
+				    
 				<div class="panel panel-success">	
 					<div class="panel-heading">
 					    <div class="btn-group pull-right">
-					    	<button type="button" class="btn btn-success" ><span class="glyphicon glyphicon-lock"></span> Cambiar contraseña </button>			
+					    	
+					    	<h4 class="modal-title" id="myModalLabel"><i class="glyphicon glyphicon-edit"></i> Editar Administrador</h4>	
 						</div>				
 					</div>
 					<br>
 					<div class="panel-body">
+						<div class="container">
+							<div class="row">
+								<div class="col-xs-12 col-sm-4 ">
+										<form class="form-horizontal" role="form" action="<?php $_SERVER['PHP_SELF']; ?>" method="POST" autocomplete="off">
 							
+							<div id="signupalert" style="display:none" class="alert alert-danger">
+								<p>Error:</p>
+								<span></span>
+							</div>
+
+							<div class="form-group">
+								<label class="col-md-3">Usuario</label>
+								<br>
+								<div class="col-md-12">
+									<input type="text" class="form-control" name="usuario" placeholder="Usuario" value="" required="">
+								</div>
+							</div>
+
+							<div class="form-group">
+								<label class="col-md-3">Nombre</label>
+								<div class="col-md-12">
+									<input type="text" class="form-control" name="nombre" placeholder="Nombre" value="" required="">
+								</div>
+							</div>
+							
+							<div class="form-group">
+								<label class="col-md-3">Celular</label>
+								<div class="col-md-12">
+									<input type="text" class="form-control" name="celular" placeholder="Celular" value="" required="">
+								</div>
+							</div>
+						
+							<div class="form-group">
+								<label class="col-md-3 ">Email</label>
+								<div class="col-md-12">
+									<input type="email" class="form-control" name="email" placeholder="Email" value="" required="">
+								</div>
+							</div>
+
+							<div class="form-group">                                      
+								<div class="col-md-9">
+									<button id="btn-signup" type="submit" class="btn btn-success"><i class="icon-hand-right"></i>Actualizar Perfil</button> 
+								</div>
+							</div>
+						</form>							
+							
+								</div>
+								<div class="col-xs-12 col-sm-4 ">
+									<div class="form-group">
+									<label class="col-md-3">Contraseña</label>
+									<div class="col-md-12">
+										<button type="button" class="btn btn-success" data-toggle="modal" data-target="#myModal2"><span class="glyphicon glyphicon-lock"></span> Cambiar contraseña </button>
+								</div>
+							</div>
+								</div>
+								<div class="col-xs-12 col-sm-4 "></div>
+							</div>
+						</div>	
 			 	 	</div>
+
 	 	 		
 				</div><!-- ModalUser -->
 				<div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
@@ -42,40 +101,34 @@
 					<div class="modal-content">
 					  <div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-						<h4 class="modal-title" id="myModalLabel"><i class="glyphicon glyphicon-edit"></i> Editar usuario</h4>
+						<h4 class="modal-title" id="myModalLabel"><i class="glyphicon glyphicon-edit"></i> Actualizar Contraseña</h4>
 					  </div>
 					  <div class="modal-body">
 						<form class="form-horizontal" method="post" id="editar_vendedor" name="editar_vendedor">
-						<div id="resultados_ajax2"></div>
 						 	<div class="form-group">
-							<label for="documento" class="col-sm-3 control-label">Documento</label>
-							<div class="col-sm-8">
-							  <input type="text" class="form-control" id="documento" name="documento">
+							<div class="col-sm-12">
+							  <input type="text" class="form-control" id="documento" placeholder="Contraseña Actual" name="documento">
 							</div>
-						  </div>
+						  	</div>
 						  
 							<div class="form-group">
-							<label for="nombre" class="col-sm-3 control-label">Nombre</label>
-							<div class="col-sm-8">
-							  <input type="text" class="form-control" id="nombre" name="nombre" required="">
+							<div class="col-sm-12">
+							  <input type="text" class="form-control" id="nombre" placeholder="Nueva Contraseña " name="nombre" required="">
 							</div>
-						  </div>
+						 	</div>
 						  
-						  <div class="form-group">
-							<label for="celular" class="col-sm-3 control-label">Celular</label>
-							<div class="col-sm-8">
-								<input type="text" class="form-control" id="celular" name="celular">
+						  	<div class="form-group">
+							<div class="col-sm-12">
+							<input type="text" class="form-control" id="celular" placeholder="Confirma Nueva Contraseña" name="celular">
 							</div>
-						  </div>	
+							</div>	
 					  </form></div>
 					  <div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-						<button type="submit" class="btn btn-primary" id="actualizar_datos">Actualizar datos</button>
+						<button type="submit" class="btn btn-primary" id="actualizar_datos">Actualizar Contraseña</button>
 					  </div>
-					  
-					</div>
-				  </div>
-				</div>	
+				
+
 			</div>
 		</div>		
 	</div>
@@ -94,12 +147,3 @@
 <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
-
-
-		
-
-
-		
-
-
-		
