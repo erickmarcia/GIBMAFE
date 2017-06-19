89964e3fed1791b4b4c81d0ad90a349ccf4747fd
Cristhian Mendoza <stmendozza@gmail.com>
2017-06-19 13:11:30 -0500

    Update reportes.php

--- codigo/reportes.php
+++ codigo/reportes.php
@@ -4,7 +4,7 @@
 	include "config.php";
 	// si el usuario esta conectado muestra el sitio de chat si no lo redirige al index para que se logee o se registre
 	if (isset($_SESSION['usuario']))
-	{		
+	{		 
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -23,16 +23,12 @@
 				</div>
 				</div>
 				<div class="contenedor-section0	 col-xs-12 col-sm-10 col-sd-10 ">
-				 <br>	     
+				 	     
 				<div class="panel panel-success">	
+
+
 					<div class="panel-heading">
-					    <div class="btn-group pull-right">
-					    	 <h3>Reportes</h3>			
-						</div>				
-					</div>
-					<br>
-					<div class="panel-body">
-						 <label id="">Reporte de:</label>
+					    <label id="">Reporte de:</label>
 				    <select>
 				    <option>Abastecimiento</option>
 				    <option>Venta</option>
@@ -43,7 +39,11 @@
 				    <option>Entrega Garantía</option>
 				    </select> de <input type="date" name="">	hasta <input type="date" name="">		
 				    <button type="button" class="btn btn-success" ><span class="glyphicon glyphicon-print"></span> Generar Reporte </button>	
+					<br>		
+					</div>
 					<br>
+					<div class="panel-body">
+						 
 			 	 	</div>
 	 	 		
 				</div>
