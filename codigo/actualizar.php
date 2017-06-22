513adfbbb8c3f7524e740d527fb9a65a4c58f227
Cristhian Mendoza <stmendozza@gmail.com>
2017-06-21 20:04:27 -0500

    Add files via upload

---
+++ codigo/actualizar.php
@@ -0,0 +1,28 @@
+<?php 
+
+$usuario=$_POST['usuario'];
+$nombre=$_POST['nombre'];
+$celular=$_POST['celular'];
+$correo=$_POST['correo'];
+
+include ('funciones.php');//llamo la funcion
+session_start();
+
+$sql = "update tb_usuarios set usuario= '$usuario', nombre= '$nombre', celular='$celular', correo='$correo' where usuario='$_SESSION[usuario]'";
+//echo $sql;
+//indico cuales seran los campos a modificar con sus respectivos valores siguiendo con los parametros que se asigno en la funcion de actualizar
+include("config.php");
+	$resultado = $conexion->query( $sql );
+	if( $conexion->affected_rows > 0 )
+		{	session_destroy();
+			session_start();
+			$_SESSION['usuario']=$usuario;
+			echo "<script>location.href='usuario.php'</script>";//me retorna a la pantalla inicial
+
+		}else{
+				echo "<script> alert ('Error: no se han actualizado los datos de la tabla en la base de datos.') </script>";
+				echo "<script>location.href='usuario.php'</script>";//me retorna a la pantalla inicial
+			}
+//echo "<script>location.href='usuario.php'</script>";//me retorna a la pantalla inicial
+
+?>
