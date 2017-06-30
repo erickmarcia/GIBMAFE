fb739d4b57b048475af8f717a8c0bddb9f0006cb
Cristhian Mendoza <stmendozza@gmail.com>
2017-06-30 10:41:56 -0500

    Update headcommon.php

--- codigo/inc/headcommon.php
+++ codigo/inc/headcommon.php
@@ -1,4 +1,5 @@
 	<link rel="shortcut icon" type="image/x-icon" href="images/sen2.png">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	 <link href="css/jquery.dataTables.min.css" rel="stylesheet">
