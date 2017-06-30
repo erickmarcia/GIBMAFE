1188b04253ca87ef542fd7612370b872f2bb3bbe
Cristhian Mendoza <stmendozza@gmail.com>
2017-06-30 10:35:28 -0500

    Update footer.php

--- codigo/inc/footer.php
+++ codigo/inc/footer.php
@@ -4,7 +4,7 @@
 			<div class="col-xs-12" >
 			<center>
 			<a href="http://regionalguaviare.blogspot.com.co" target="_blank" style="color: #ecf0f1">SENA</a>	
-			<p style="color: #ecf0f1">Centro de Desarrollo Agroindustrial Turístico y Tecnológico Regional Guaviare</p>	
+			<p style="color: #ecf0f1">Centro de Desarrollo Agroindustrial Turístico y Tecnol&oacute;gico Regional Guaviare</p>	
 			<p style="color: yellow">Stmendozza &copy; <br>© 2017 - Boutique Maria Fernanda.</p>
 			</center>
 			</div>
