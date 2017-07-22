0fc7f71fc80345df4d57f5c1e764a800a3dbd91f
Cristhian Mendoza <stmendozza@gmail.com>
2017-07-22 12:46:53 -0500

    Add files via upload

--- codigo/inc/menu.php
+++ codigo/inc/menu.php
@@ -1,7 +1,7 @@
 				<a class="btn-menu">Boutique Maria Fernanda<i class="icono glyphicon glyphicon-align-justify"></i></a>
 				<ul class="menu">
 
-					<li ><a href="usuario.php" class="verde"><i class="verde icono izquierda glyphicon glyphicon-thumbs-up" ></i>Bienvenid@!! <?php echo $_SESSION['nombre']; ?></a></li>
+				
 					<li ><a href="stockdispo.php"><i class="icono izquierda glyphicon glyphicon-sort-by-alphabet" ></i>Stock disponible</a></li>
 					
 					<li  ><a href="movimientos.php"><i class="icono izquierda glyphicon glyphicon-log-in" ></i>Movimientos</a></li>
@@ -13,7 +13,7 @@
 					</li>	
 					<li ><a href="#"><i class="icono izquierda glyphicon glyphicon-user" ></i>Administrador<i class="icono derecha glyphicon glyphicon-menu-down" ></i></a>
 						<ul >
-							<li ><a href="usuario.php">Usuario</a></li>
+							<li ><a href="usuario.php"><?php echo $_SESSION['nombre']; ?></a></li>
 							<li ><a href="reportes.php">Reportes</a></li>
 						</ul>	
 					</li>
